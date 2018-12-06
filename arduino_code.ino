301863437edf1dc681b13bdd68b5388552df319f
Hell0w <helloosong@gmail.com>
2018-12-06 13:02:05 +0100

    Mise à jour de l'arduino

--- arduino_code.ino
+++ arduino_code.ino
@@ -1,47 +1,49 @@
 #include <Servo.h>
 #include <Ultrasonic.h>
 
-Ultrasonic ultrasonic(7); //Capteur de distance branché sur 7
-Servo myservo;
-int pos = 0; // Position de la plateforme rotative
-int affpos = 0;
+int robotx = 0;
+int roboty = 0;
 int pin_servo = 5; //PIN du servomoteur
+int degre = 0;
+Ultrasonic *capteurs[4];
+Servo myservo;
 
-void setup() {
+void setup() {  
+  myservo.attach(pin_servo); // On démarre le servomoteur
+  Serial.begin(9600); // On commence à parler à l'ordi
 
-	myservo.attach(pin_servo);
-	Serial.begin(9600);
+  Ultrasonic capteur1(1);
+  Ultrasonic capteur2(2);
+  Ultrasonic capteur3(3);
+  Ultrasonic capteur4(4);
 
+  
+  capteurs[0] = &capteur1;
+  capteurs[1] = &capteur2;
+  capteurs[2] = &capteur3;
+  capteurs[3] = &capteur4;
+/*
+  Ultrasonic capteurs[4];
+  capteurs[0] = capteur1;
+  capteurs[1] = capteur2;
+  capteurs[2] = capteur3;
+  capteurs[3] = capteur4;*/
 }
 
 void loop() {
-	
-	for (pos = 0; pos <= 180; pos += 5) {	 
-		// Rotation 0-180 degrés par pas de 5 degrés
-		myservo.write(pos);	// le servo va à la position "pos" 
-		delay(150);	// on attend 15ms pour que le servo atteigne la position "pos"
+  envoi_Mesure();
 
-		affpos=pos-90; // Position à envoyer au Raspberry Pi : on passe de 0-180 à -90 - 90
-		printDistance(affpos); 
-	}
-	for (pos = 180; pos >= 0; pos -= 5) {
-		// le retour: 180-0 degrés par pas de 5°
-		myservo.write(pos);
-		delay(150);
-
-		affpos=pos-90;			//Position à envoyer au Raspberry pi
-		printDistance(affpos); 
-	}
 }
- 
-void printDistance(int pos) { 
-	long RangeInCentimeters; 
-	RangeInCentimeters = ultrasonic.MeasureInCentimeters(); 	// On fait la mesure
-	if(RangeInCentimeters < 516) { 		// Le capteur renvoie 517 si il ne détecte rien ou si il beug --> pas d'envoi
-		Serial.print(RangeInCentimeters); //0~400cm 
-	}
-	Serial.print(" cm, ");
-	Serial.print(pos);
-	Serial.println(" deg");
-	//Exemple: 50 cm, 45 deg
+
+void envoi_Mesure() {
+  for(int i = 0; i < 4; i++) {
+    long RangeInCentimeters; 
+    RangeInCentimeters = capteurs[i]->MeasureInCentimeters();   // On fait la mesure
+    if(RangeInCentimeters < 516) {    // Le capteur renvoie 517 si il ne détecte rien ou si il beug --> pas d'envoi
+      //Serial.println(robotx, ", ", roboty, ", ", degre, ", ", RangeInCentimeters);
+      Serial.print(robotx);
+    }
+  }
+  degre += 5;
+  myservo.write(degre);
 }
