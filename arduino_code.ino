7c7b596e366d44281c19f152a9db3e45f0ea14a6
Hell0w <helloosong@gmail.com>
2018-12-13 12:57:44 +0100

    Même chose

--- arduino_code.ino
+++ arduino_code.ino
@@ -15,31 +15,38 @@ Ultrasonic capteur3(12);
 void setup() {  
   myservo.attach(pin_servo); // On démarre le servomoteur
   Serial.begin(9600); // On commence à parler à l'ordi
-  
 }
 
 void loop() {
   envoi_Mesure();
-
 }
 
 void envoi_Mesure() {
+  Serial.print(robotx);
+  Serial.print(", ");
+  Serial.print(roboty);
+  Serial.print(", ");
+  Serial.print(degre);
+  Serial.print(", ");
   Serial.println(capteur1.MeasureInCentimeters());
+  
+  Serial.print(robotx);
+  Serial.print(", ");
+  Serial.print(roboty);
+  Serial.print(", ");
+  Serial.print(degre+90);
+  Serial.print(", ");
   Serial.println(capteur2.MeasureInCentimeters());
+  
+  Serial.print(robotx);
+  Serial.print(", ");
+  Serial.print(roboty);
+  Serial.print(", ");
+  Serial.print(degre+180);
+  Serial.print(", ");
   Serial.println(capteur3.MeasureInCentimeters());
   //Serial.println(capteur4.MeasureInCentimeters());
 
-  /*
-  for(int i = 0; i < 1; i++) {
-    long RangeInCentimeters; 
-    //RangeInCentimeters = capteurs[i]->MeasureInCentimeters();   // On fait la mesure
-    RangeInCentimeters = capteur1.MeasureInCentimeters();
-    if(RangeInCentimeters < 516) {    // Le capteur renvoie 517 si il ne détecte rien ou si il beug --> pas d'envoi
-      Serial.println((robotx, ", ", roboty, ", ", degre, ", ", RangeInCentimeters));
-      Serial.println(RangeInCentimeters);
-    }
-  }
-  */
   if (degre == 180) {
     degre = 0;
   }
